838da63196b5a4088a5d380586d351ba2b2ddd46
Dyson Parkes <dyson@berserkir.net>
2025-04-24 17:01:38 +1200

    Begin adding sharepoint functions

--- Start-TenantConfiguration/Start-TenantConfiguration.psm1
+++ Start-TenantConfiguration/Start-TenantConfiguration.psm1
@@ -6,7 +6,69 @@
 # comments: 
 # --------------------------------------------------------
 
-
+function Start-TenantConfiguration {
+    #region Parameters (domain)
+    param(
+    [Parameter(Mandatory=$true, HelpMessage="What domain is this for?")][string]$Domain,
+    [Parameter(Mandatory=$false, HelpMessage="Configure default Exchange rules?")][bool]$ExchangeRules = $true,
+    [Parameter(Mandatory=$false, HelpMessage="Configure default Sharepoint folder?")][bool]$Sharepoint = $false
+    )
+    #endregion
+    #region Check if the script is running with administrative privileges
+    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Host "This script requires administrative privileges. Please run it as an administrator."
+        exit
+    }
+    #endregion
+    #region Exchange Tasks
+    if($ExchangeRules) {
+        Write-Host "Configuring Exchange Online rules for $Domain"
+        #region Import ExchangeOnlineManagement module if available, install if not
+        if (-not (Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
+            Write-Host "Exchange Online Management module is not installed. Installing..."
+            Install-Module -Name ExchangeOnlineManagement -Force -AllowClobber
+            Import-Module ExchangeOnlineManagement
+        } else {
+            Import-Module ExchangeOnlineManagement
+        }
+        #endregion
+        #region Connect to exchange online and check if the connection was successful
+        Connect-ExchangeOnline
+        if (-not (Get-ExchangeOnlineConnection)) {
+            Write-Host "Failed to connect to Exchange Online. Please check your credentials and try again."
+            exit
+        } else {
+            Write-Host "Connected to Exchange Online successfully."
+        }
+        #endregion
+        #region Configure Exchange Rules
+        Write-Host "Configuring AntiSpoofing Rule for $Domain"
+        Set-AntiSpoofing -domain $Domain
+        
+        Write-Host "Configuring Blocklist Rule for $Domain"
+        Set-Blocklist -domain $Domain
+        
+        Write-Host "Configuring AntiSpam Rule for $Domain"
+        Set-AntiSpam -domain $Domain
+        
+        Write-Host "Configuring External Sender Disclaimer Rule for $Domain"
+        Set-ExternalDisclaimer -domain $Domain
+        
+        Write-Host "Configuring Blocked Attachment Rule for $Domain"
+        Set-AttachmentFilter
+        #endregion
+        #region Disconnect from Exchange Online
+        Disconnect-ExchangeOnline -Confirm:$false
+        #endregion
+    } else {
+        Write-Host "Skipping Exchange Online configuration."
+    }
+    #endregion
+    #region Sharepoint Tasks
+    Write-Host "Configuring Sharepoint Tenant for $Domain"
+    
+    #endregion
+    }
 
 #region Configure AntiSpoofing Rule
 function Set-AntiSpoofing {
@@ -48,33 +110,6 @@ function Set-AttachmentFilter {
 New-TransportRule -Name "Block nasty files" -Priority 4 -Mode Enforce -FromScope NotInOrganization -DeleteMessage $true -AttachmentExtensionMatchesWords "*htm*"
 }
 #endregion
-
-function Start-TenantConfiguration {
-#region Parameters (domain)
-param(
-[Parameter(Mandatory=$true, HelpMessage="What domain is this for?")][string]$Domain
-# The drive letter of the windows installer
-)
-#endregion
-# Import Exchange Online module
-Import-Module ExchangeOnlineManagement
-# Connect to exchange online
-Connect-ExchangeOnline
-Write-Host "Configuring AntiSpoofing Rule for $Domain"
-Set-AntiSpoofing -domain $Domain
-
-Write-Host "Configuring Blocklist Rule for $Domain"
-Set-Blocklist -domain $Domain
-
-Write-Host "Configuring AntiSpam Rule for $Domain"
-Set-AntiSpam -domain $Domain
-
-Write-Host "Configuring External Sender Disclaimer Rule for $Domain"
-Set-ExternalDisclaimer -domain $Domain
-
-Write-Host "Configuring Blocked Attachment Rule for $Domain"
-Set-AttachmentFilter
-}
 
 Export-ModuleMember -Function *
 Export-ModuleMember -Variable *
