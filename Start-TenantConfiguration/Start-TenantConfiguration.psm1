cd99e337850e500a79ffc7206d13e1616a778021
Dyson Parkes <dyson@berserkir.net>
2025-04-29 12:30:30 +1200

    Reorder exchange stuff

--- Start-TenantConfiguration/Start-TenantConfiguration.psm1
+++ Start-TenantConfiguration/Start-TenantConfiguration.psm1
@@ -7,11 +7,13 @@
 # --------------------------------------------------------
 
 function Start-TenantConfiguration {
-    #region Parameters (domain)
+    #region Parameters
     param(
-    [Parameter(Mandatory=$true, HelpMessage="What domain is this for?")][string]$Domain,
+    [Parameter(Mandatory=$true, HelpMessage="What domain is this for?")][string]$MailDomain,
     [Parameter(Mandatory=$false, HelpMessage="Configure default Exchange rules?")][bool]$ExchangeRules = $true,
-    [Parameter(Mandatory=$false, HelpMessage="Configure default Sharepoint folder?")][bool]$Sharepoint = $false
+    [Parameter(Mandatory=$false, HelpMessage="Configure Sharepoint?")][bool]$Sharepoint = $false,
+    [Parameter(Mandatory=$false, HelpMessage="What is the Sharepoint Admin URL?")][string]$SharepointUrl = "",
+    [Parameter(Mandatory=$false, HelpMessage="Configure default Sharepoint folder?")][bool]$SharepointFolder = $false
     )
     #endregion
     #region Check if the script is running with administrative privileges
@@ -22,7 +24,7 @@ function Start-TenantConfiguration {
     #endregion
     #region Exchange Tasks
     if($ExchangeRules) {
-        Write-Host "Configuring Exchange Online rules for $Domain"
+        Write-Host "Configuring Exchange Online rules for $MailDomain"
         #region Import ExchangeOnlineManagement module if available, install if not
         if (-not (Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
             Write-Host "Exchange Online Management module is not installed. Installing..."
@@ -39,33 +41,55 @@ function Start-TenantConfiguration {
             exit
         } else {
             Write-Host "Connected to Exchange Online successfully."
+            #region Configure Exchange Rules
+            Write-Host "Configuring AntiSpoofing Rule for $MailDomain"
+            Set-AntiSpoofing -domain $MailDomain
+            
+            Write-Host "Configuring Blocklist Rule for $MailDomain"
+            Set-Blocklist -domain $MailDomain
+            
+            Write-Host "Configuring AntiSpam Rule for $MailDomain"
+            Set-AntiSpam -domain $MailDomain
+            
+            Write-Host "Configuring External Sender Disclaimer Rule for $MailDomain"
+            Set-ExternalDisclaimer -domain $MailDomain
+            
+            Write-Host "Configuring Blocked Attachment Rule for $MailDomain"
+            Set-AttachmentFilter
+            #endregion
+            #region Disconnect from Exchange Online
+            Disconnect-ExchangeOnline -Confirm:$false
+            #endregion
         }
-        #endregion
-        #region Configure Exchange Rules
-        Write-Host "Configuring AntiSpoofing Rule for $Domain"
-        Set-AntiSpoofing -domain $Domain
-        
-        Write-Host "Configuring Blocklist Rule for $Domain"
-        Set-Blocklist -domain $Domain
-        
-        Write-Host "Configuring AntiSpam Rule for $Domain"
-        Set-AntiSpam -domain $Domain
-        
-        Write-Host "Configuring External Sender Disclaimer Rule for $Domain"
-        Set-ExternalDisclaimer -domain $Domain
-        
-        Write-Host "Configuring Blocked Attachment Rule for $Domain"
-        Set-AttachmentFilter
-        #endregion
-        #region Disconnect from Exchange Online
-        Disconnect-ExchangeOnline -Confirm:$false
         #endregion
     } else {
         Write-Host "Skipping Exchange Online configuration."
     }
     #endregion
     #region Sharepoint Tasks
-    Write-Host "Configuring Sharepoint Tenant for $Domain"
+    if($Sharepoint) {
+        Write-Host "Connecting to Sharepoint for $SharepointUrl"
+        #region Import Sharepoint module if available, install if not
+        if (-not (Get-Module -ListAvailable -Name Microsoft.Online.SharePoint.PowerShell)) {
+            Write-Host "Microsoft.Online.SharePoint.PowerShell module is not installed. Installing..."
+            Install-Module -Name Microsoft.Online.Sharepoint.Powershell -Force -AllowClobber
+            Import-Module Microsoft.Online.SharePoint.PowerShell -UseWindowsPowerShell
+        } else {
+            Import-Module Microsoft.Online.SharePoint.PowerShell -UseWindowsPowerShell
+        }
+        #endregion
+        #region Connect to Sharepoint Online and check if the connection was successful
+        Connect-SpoService -Url "$sharepointUrl"
+        if (-not (Get-SPOSite)) {
+            Write-Host "Failed to connect to Sharepoint Online. Please check your credentials and try again."
+            exit
+        } else {
+            Write-Host "Connected to Sharepoint Online successfully."
+        }
+        #endregion
+    } else {
+    Write-Host "Skipping Sharepoint setup for $SharepointUrl"
+    }
     
     #endregion
     }
@@ -73,15 +97,15 @@ function Start-TenantConfiguration {
 #region Configure AntiSpoofing Rule
 function Set-AntiSpoofing {
 param(
-[string[]]$Domain
+[string[]]$MailDomain
 )
-New-TransportRule -Name "Anti-Spoofing" -Priority 0 -Mode Enforce -SenderAddressLocation Header -SenderDomainIs $domain -RejectMessageEnhancedStatusCode 5.7.1 -RejectMessageReasonText "Block email due to the unsafe anti-spoofing rule" -FromScope NotInOrganization -StopRuleProcessing $false
+New-TransportRule -Name "Anti-Spoofing" -Priority 0 -Mode Enforce -SenderAddressLocation Header -SenderDomainIs $MailDomain -RejectMessageEnhancedStatusCode 5.7.1 -RejectMessageReasonText "Block email due to the unsafe anti-spoofing rule" -FromScope NotInOrganization -StopRuleProcessing $false
 }
 #endregion
 #region Configure Blocklist Rule
 function Set-Blocklist {
 param(
-[string[]]$Domain
+[string[]]$MailDomain
 )
 New-TransportRule -Name 'Block List' -Priority 1 -Mode Enforce -DeleteMessage $true -SenderAddressLocation Header -RecipientAddressType Resolved -FromAddressContainsWords {} -Enabled $false
 }
@@ -89,7 +113,7 @@ New-TransportRule -Name 'Block List' -Priority 1 -Mode Enforce -DeleteMessage $t
 #region Configure AntiSpam Rule
 function Set-AntiSpam {
 param(
-[string[]]$Domain
+[string[]]$MailDomain
 )
 New-TransportRule -Name 'Spam' -Priority 2 -Mode Enforce -SenderAddressLocation Header -RecipientAddressType Resolved -FromAddressContainsWords 'postmaster','mailer-daemon' -DeleteMessage $true
 }
@@ -97,7 +121,7 @@ New-TransportRule -Name 'Spam' -Priority 2 -Mode Enforce -SenderAddressLocation 
 #region Configure External Sender Disclaimer Rule
 function Set-ExternalDisclaimer {
 param(
-[string[]]$Domain
+[string[]]$MailDomain
 )
 $disclaimer = "<p style='background-color:#FFEB9C; width: 850px; border:2px; border-top-style:solid; border-bottom-style:solid; border-color:#FF0000; padding: 0.3em;'><span style='font-size:9pt;font-family:Arial;color:red'><b>CAUTION:</span></b><span style='font-size:9pt;font-family:Arial;color:black'> This email originated outside your Organisation.  <b>DO NOT CLICK</b> on links, attachments, or action requests unless you recognise the sender and know the content is safe.  &nbsp;⚠  If you think it is suspicious, please <b>REPORT IT</b> to your manager</span>.</p>"
 New-TransportRule -Name 'External Sender Disclaimer' -Priority 4 -Mode Enforce -SenderAddressLocation Header -RecipientAddressType Resolved -FromScope NotInOrganization -SentToScope InOrganization -ExceptIfSubjectOrBodyContainsWords "This email originated outside your Organisation" -ApplyHtmlDisclaimerFallbackAction Wrap -ApplyHtmlDisclaimerLocation Prepend -ApplyHtmlDisclaimerText $disclaimer
@@ -110,5 +134,14 @@ New-TransportRule -Name "Block nasty files" -Priority 4 -Mode Enforce -FromScope
 }
 #endregion
 
+#region Configure Sharepoint Folder
+function Set-SharepointFolder {
+param(
+[string]$MailDomain
+) 
+
+$ctx.ExecuteQuery()
+$folderItem = $ctx.Web.Lists.GetByTitle("Documents").RootFolder.Folders.Add($folderName)
+#endregion
 Export-ModuleMember -Function *
 Export-ModuleMember -Variable *
