bb710e7d35e33e14c2049577810e69714b2ec0a9
Dyson Parkes <dyson@berserkir.net>
2025-05-29 10:41:49 +1200

    Add parameters to exclude disabled users and confirm OU before activating

--- Match-EntraAD/Match-EntraAD.ps1
+++ Match-EntraAD/Match-EntraAD.ps1
@@ -15,7 +15,9 @@
 #>
 # Specify the additional UPN suffix to be added to local AD
 [Parameter(Mandatory=$false, HelpMessage="What UPN suffix do you want to add to local AD?")][string]$upn
-[Parameter(Mandatory=$false, HelpMessage="What OU do you want to search for local AD users? (ie OU=Users_Staff, DC=domain,DC=local)")][string]$OU
+[Parameter(Mandatory=$true, HelpMessage="What OU do you want to search for local AD users? (ie OU=Users_Staff, DC=domain,DC=local)")][string]$OU
+[Parameter(Mandatory=$false, HelpMessage="Should the script only update enabled users? (true/false)")][bool]$EnabledOnly = $true
+[Parameter(Mandatory=$false, HelpMessage="Where do you want to export the local AD users to?")][string]$csvpath = "C:\Testing\ImmutableIDs.txt"
 
 #region check if the script is running with administrative privileges
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
@@ -41,6 +43,12 @@ if (-not (Get-Module -ListAvailable -Name ActiveDirectory)) {
     Import-Module ActiveDirectory
 }
 #endregion
+#region check if the CSV file path exists
+if (-not (Test-Path -Path $csvpath)) {
+    Write-Host "CSV file path does not exist. Creating directory..."
+    New-Item -ItemType Directory -Path (Split-Path -Path $csvpath) -Force
+}
+#endregion
 #region Add UPN suffix to local AD if specified
 if ($upn) {
     $localupn = (Get-ADForest).UPNSuffixes[0]
@@ -50,10 +58,13 @@ if ($upn) {
     Write-Host "Added UPN suffix $upn to local AD"
     Write-Host "Local AD UPN suffixes are now $($localAD.UPNSuffixes)"
     # Update local AD users with the new UPN suffix
-    if (-not $OU) {
-        $OU = Read-Host 'Enter the full OU to search for local AD users (e.g. "OU=Users_Staff,DC=domain,DC=local")'
-    }   
-    $ADusers = Get-ADUser -Filter * -SearchBase $OU -properties *
+    if ($EnabledOnly) {
+        Write-Host "Updating UPN for enabled users only in OU $OU"
+        $ADusers = Get-ADUser -Filter 'enabled -eq "true"' -SearchBase $OU -properties *
+    } else {
+        Write-Host "Updating UPN for all users in OU $OU"
+        $ADusers = Get-ADUser -Filter * -SearchBase $OU -properties *
+    }
     foreach ($ADuser in $ADusers) {
         $newUPN = "$($ADuser.SamAccountName)@$upn"
         Set-ADUser -Identity $ADuser -UserPrincipalName $newUPN
@@ -79,13 +90,16 @@ if (-not (Get-MgContext)) {
 # The ImmutableId is the ObjectGUID of the local AD user, converted to a base64 string
 # The ObjectGUID is the unique identifier for the user in local AD
 # A powershell version of ldifde -f export.txt -r "(Userprincipalname=*)" -l "objectGuid, userPrincipalName"
-if (-not $OU){
-    $OU = Read-Host 'Enter the full OU to search for local AD users (e.g. "OU=Users_Staff,DC=domain,DC=local")'
+
+if ($EnabledOnly) {
+    Write-Host "Exporting enabled users only from OU $OU"
+    Get-ADUser -Filter 'enabled -eq "true"' -SearchBase $OU -properties * | Select-Object UserPrincipalName,ObjectGUID,@{n="ImmutableID";e={[System.Convert]::ToBase64String($_.ObjectGUID.tobytearray())} },EmailAddress | Export-CSV $csvpath -NoClobber -NoTypeInformation
+} else {
+    Get-ADUser -Filter * -SearchBase $OU -properties * | Select-Object UserPrincipalName,ObjectGUID,@{n="ImmutableID";e={[System.Convert]::ToBase64String($_.ObjectGUID.tobytearray())} },EmailAddress | Export-CSV $csvpath -NoClobber -NoTypeInformation
 }
-Get-ADUser -Filter * -SearchBase $OU -properties * | Select-Object UserPrincipalName,ObjectGUID,@{n="ImmutableID";e={[System.Convert]::ToBase64String($_.ObjectGUID.tobytearray())} },EmailAddress |Export-CSV C:\Testing\immutableIDs.csv -NoClobber -NoTypeInformation
 #endregion
 #region Import the CSV file
-$localusers = Import-Csv C:\Testing\immutableIDs.txt
+$localusers = Import-Csv $csvpath
 #endregion
 #region Loop through each local AD user and match it with the Entra ID user
 foreach ($localuser in $localusers) {
